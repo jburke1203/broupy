34a4af2511f0f396a0c85f73ab5069649574fa20
James Burke <james.burke1203@gmail.com>
2016-08-07 15:57:17 +0200

    First FT specced out in comments and now uses unittest

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,7 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8080/')
+class NewVisitorTest(unittest.TestCase):
 
-print(browser.find_elements_by_tag_name('p')[0].text)
-assert 'Hello, World!' == browser.find_elements_by_tag_name('p')[0].text
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_search_a_city(self):
+        # Edith has heard about a cool new online travel site
+        # She goes to check out its homepage
+        self.browser.get('http://127.0.0.1:8080/')
+
+        # She notices the page title and header mention the title
+        assert 'Travel Share' in self.browser.title
+
+        # She is invited to search or select a city
+
+        # She types in "Berlin, Germany" into a text box
+        # Edith would like to visit Berlin
+
+        # When she hits enter, the page changes, and now
+        # she can see all the tour plans for Berlin
+
+        # Edith has a look at the first one on the list
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
